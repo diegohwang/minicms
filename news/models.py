7924cac5a87c363f19ebee983afa737ece1ea68f
diegohwang <diegohwang@163.com>
2016-05-13 14:55:40 +0800

    集成百度编辑器后续相关操作

--- news/models.py
+++ news/models.py
@@ -3,6 +3,7 @@
 from __future__ import unicode_literals#必须放在首行
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
+from DjangoUeditor.models import UEditorField
 
 
 @python_2_unicode_compatible
@@ -25,7 +26,10 @@ class Article(models.Model):
     title = models.CharField('标题', max_length=128)
     slug = models.CharField('网址', max_length=128, db_index=True)
     author = models.ForeignKey('auth.User', blank=True, null=True, verbose_name='作者')
-    content = models.TextField('内容', default='', blank=True)
+    #content = models.TextField('内容', default='', blank=True)
+    content = UEditorField('内容', height=300, width=1000, default=u'',
+        blank=True, imagePath='uploads/images/', toolbars='besttome',
+        filePath='uploads/files/')
     published = models.BooleanField('正式发布', default=True)
     
     pub_date = models.DateTimeField('发表时间', auto_now_add=True,editable=True)

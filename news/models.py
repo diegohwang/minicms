dfca5d2fe92403e4b8470dc57288134e27a9e34f
huangjm <huangjm@fun.tv>
2016-05-13 12:43:49 +0800

    修改字段长度

--- news/models.py
+++ news/models.py
@@ -7,8 +7,8 @@ from django.utils.encoding import python_2_unicode_compatible
 
 @python_2_unicode_compatible
 class Column(models.Model):
-    name = models.CharField('栏目名称', max_length=256)
-    slug = models.CharField('栏目网址', max_length=256, db_index=True)
+    name = models.CharField('栏目名称', max_length=128)
+    slug = models.CharField('栏目网址', max_length=128, db_index=True)
     intro = models.TextField('栏目简介', default='')
     
     def __str__(self):
@@ -22,9 +22,9 @@ class Column(models.Model):
 @python_2_unicode_compatible
 class Article(models.Model):
     column = models.ManyToManyField(Column, verbose_name='归属栏目')
-    title = models.CharField('标题', max_length=256)
-    slug = models.CharField('网址', max_length=256, db_index=True)
-    author = models.CharField('auth.User', blank=True, null=True, verbose_name='作者')
+    title = models.CharField('标题', max_length=128)
+    slug = models.CharField('网址', max_length=128, db_index=True)
+    author = models.ForeignKey('auth.User', blank=True, null=True, verbose_name='作者')
     content = models.TextField('内容', default='', blank=True)
     published = models.BooleanField('正式发布', default=True)
     

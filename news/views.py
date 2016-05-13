a4f23c9e2b6a048f144b5ca42b276eb0d0a4a40a
diegohwang <diegohwang@163.com>
2016-05-13 16:35:28 +0800

    correct spelling error

--- news/views.py
+++ news/views.py
@@ -16,7 +16,7 @@ def column_detail(request, column_slug):
     column = Column.objects.get(slug=column_slug)
     return render(request, 'news/column.html', {'column': column})
 
-def article_detail(requset, article_slug):
+def article_detail(request, article_slug):
     #return HttpResponse('article slug: ' + article_slug)
     article = Article.objects.filter(slug=article_slug)[0]
     return render(request, 'news/article.html', {'article': article})
